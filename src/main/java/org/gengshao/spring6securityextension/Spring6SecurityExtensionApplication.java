a0b1d5d5eaabc233b910da83d3b355327638ca02
chengsean <chengsz7890@gmail.com>
2023-11-09 11:32:35 +0800

    代码提交

---
+++ src/main/java/org/gengshao/spring6securityextension/Spring6SecurityExtensionApplication.java
@@ -0,0 +1,13 @@
+package org.gengshao.spring6securityextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6SecurityExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6SecurityExtensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gengshao/spring6securityextension/Spring6SecurityExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gengshao.spring6securityextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6SecurityExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
